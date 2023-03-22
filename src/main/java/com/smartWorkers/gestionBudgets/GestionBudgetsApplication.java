8b6ff6a86248d0b5660df1e8093137a337916830
hakimBarbaria <hakimbarbaria589@gmail.com>
2023-03-22 10:24:58 -0700

    initialiser le projet

---
+++ src/main/java/com/smartWorkers/gestionBudgets/GestionBudgetsApplication.java
@@ -0,0 +1,13 @@
+package com.smartWorkers.gestionBudgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBudgetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBudgetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartWorkers/gestionBudgets/GestionBudgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartWorkers.gestionBudgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBudgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
