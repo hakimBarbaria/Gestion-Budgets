1270e1bee88a1ca175697ac257753cb2f78fbc0b
hakimBarbaria <hakimbarbaria589@gmail.com>
2023-05-01 19:50:53 -0700

    some changes in dashboard style and adding the numbers

--- src/main/java/com/smartWorkers/gestionBudgets/controllers/ApplicationController.java
+++ src/main/java/com/smartWorkers/gestionBudgets/controllers/ApplicationController.java
@@ -8,28 +8,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.smartWorkers.gestionBudgets.entities.Categories;
 import com.smartWorkers.gestionBudgets.entities.Transactions;
+import com.smartWorkers.gestionBudgets.services.CategoriesService;
 import com.smartWorkers.gestionBudgets.services.TransactionsService;
 
 @Controller
 public class ApplicationController {
   @Autowired
   TransactionsService transactionsService;
+  @Autowired
+  CategoriesService categorieService;
 
   @RequestMapping("/Dashboard")
   public String RedirectToDashboard(ModelMap modelMap) {
     List<Float> expensesCount = transactionsService.getExpensesCountsByMonth();
     List<Float> incomeCount = transactionsService.getIncomeCountsByMonth();
     List<Transactions> ALLtransactions = transactionsService.getTransactions();
-    int C=0;
-    Iterator it= ALLtransactions.iterator();
+    List<Categories> Categories = categorieService.getCategories();
+    
+    int CC=0;
+    Iterator it= Categories.iterator();
     while(it.hasNext()) {
-    	C++;
+    	CC++;
     	it.next();
+    }
+    
+    int C=0;
+    Iterator it2= ALLtransactions.iterator();
+    while(it2.hasNext()) {
+    	C++;
+    	it2.next();
     }
     modelMap.addAttribute("expensesCount", expensesCount);
     modelMap.addAttribute("incomeCount", incomeCount);
     modelMap.addAttribute("nombreTransactions",C);
+    modelMap.addAttribute("nombreCategories",CC);
     return "dashboard";
   }
 
