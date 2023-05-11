15dd647f6d0abbe0b134137d18e39b4a419702dc
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2023-05-11 08:07:12 +0100

    add getBudgets method

--- src/main/java/com/smartWorkers/gestionBudgets/services/BudgetsServiceImpl.java
+++ src/main/java/com/smartWorkers/gestionBudgets/services/BudgetsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.smartWorkers.gestionBudgets.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,14 +9,19 @@ import com.smartWorkers.gestionBudgets.dao.BudgetsRepository;
 import com.smartWorkers.gestionBudgets.entities.Budgets;
 
 @Service
-public class BudgetsServiceImpl implements BudgetsService{
+public class BudgetsServiceImpl implements BudgetsService {
 
-	@Autowired
-	BudgetsRepository budgetsRepository;
-	
-	@Override
-	public void addBudgets(Budgets budget) {
-		 budgetsRepository.save(budget);
-	}
+  @Autowired
+  BudgetsRepository budgetsRepository;
+
+  @Override
+  public void addBudgets(Budgets budget) {
+    budgetsRepository.save(budget);
+  }
+
+  @Override
+  public List<Budgets> getBudgets() {
+    return budgetsRepository.findAll();
+  }
 
 }
