3a992b3ca68b17b9c89007f9f810ea879aa4df40
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2023-05-11 05:59:45 +0100

    remove unused imports

--- src/main/java/com/smartWorkers/gestionBudgets/controllers/transactionConrtoller.java
+++ src/main/java/com/smartWorkers/gestionBudgets/controllers/transactionConrtoller.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
